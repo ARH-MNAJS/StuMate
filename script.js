454e4c40e46dc3099886ea044e0f813a5fc01936
ARH-MNAJS <arhmnajs@gmail.com>
2024-06-07 01:27:18 +0530

    bawasir

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-require('dotenv').config();
-console.log(process.env.API_KEY);
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI("AIzaSyDxAwSaHhO_NNg6B-ygGXRGrSB6K9B-7OM");
+
+async function run() {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+  const prompt = "Write a story about a magic backpack."
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
---
+++ src.js
@@ -0,0 +1,13 @@
+
+const operations = {
+    sum: function sum(a,b) {
+        let c = a+b;
+        return c;
+        },
+    product: function product(c) {
+        let d = c*2;
+        return d;
+    }
+}
+
+console.log(operations.product(3))
